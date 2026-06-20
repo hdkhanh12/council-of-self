d78e116fbc654f94f2a9887bd5a924a6fa8c560f
unknown <hoangduckhanhtn@gmail.com>
2026-06-21 04:15:30 +0700

    Latest fixing 415

--- frontend/src/app/admin/page.tsx
+++ frontend/src/app/admin/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
+
 import { useEffect, useState } from 'react'
 import { fetchWithAuth } from '@/lib/apiClient'
 import ProtectedRoute from '@/components/ProtectedRoute'
